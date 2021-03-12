45f006ca4449d8fbd92cfeab40fe7ae967a03f5f
aznic95 <aznic95@gmail.com>
2021-03-12 11:45:28 +0900

    update meta

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,10 @@ export default function Home() {
       <Head>
         <title>수염난친구들 :메인</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='naver-site-verification'
+          content='9d49a02a757cc3134f21a2242219cb37d0471427'
+        />
       </Head>
       <Layout>
         <VideoHero />
